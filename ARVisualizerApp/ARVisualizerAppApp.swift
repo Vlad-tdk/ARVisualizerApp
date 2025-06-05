65a1c78492224300e0390d4f29e9e8253e60d592
Vlad <xalif80@gmail.com>
2025-06-05 21:07:06 +0200

    Initial Commit

---
+++ ARVisualizerApp/ARVisualizerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARVisualizerAppApp.swift
+//  ARVisualizerApp
+//
+//  Created by Vladimir Martemianov on 5. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct ARVisualizerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
